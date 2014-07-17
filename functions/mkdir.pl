5186722e4ead6ddfb78648b9d45893deeb066d85
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-07-18 01:03:38 +0200

    + mkdir

---
+++ functions/mkdir.pl
@@ -0,0 +1,11 @@
+use warnings;
+use strict;
+
+use File::Path qw(rmtree make_path);
+
+
+rmtree     'new-directory' if -d 'new-directory';
+mkdir      'new-directory' or die "could not create new-directory";
+
+mkdir      'another-directory/sub-directory' or print "mkdir cannot recursively create directories, use File::Path->make_path instead\n";
+make_path  'another-directory/sub-directory' or die;
