75c4a8a5b374788178a40f4e9e3b65233f6d30e2
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-08-06 11:18:54 +0200

    Demonstrate block

--- functions/map.pl
+++ functions/map.pl
@@ -10,3 +10,28 @@ my @words = qw(one two three four five);
 my @uppercase_words = map {uc} @words;
 
 print join "\n", @uppercase_words;
+
+# --
+
+print "\n--\n";
+
+my @words_german = map { $_ eq 'one'   and 'eins' or
+                         $_ eq 'two'   and 'zwei' or
+                         $_ eq 'three' and 'drei' or
+                         $_ eq 'four'  and 'four' or
+                        '?'}  @words;
+
+print join "\n", @words_german;
+
+__END__
+ONE
+TWO
+THRE
+FOUR
+FIVE
+--
+eins
+zwei
+drei
+four
+?
