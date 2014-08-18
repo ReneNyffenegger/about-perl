50f7bcb4fd38b3a4295cf25d6c9a582d33d56726
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-08-18 17:53:27 +0200

    || and //

---
+++ operators/slash_slash.pl
@@ -0,0 +1,14 @@
+use warnings;
+use strict;
+use feature 'say';
+
+my $p;
+my $q  = "foo";
+my $r  = "";
+my $t  = "0";
+
+
+say $p // "bar"; # bar
+say $q // "bar"; # foo
+say $r // "bar"; #
+say $t // "bar"; # 0
