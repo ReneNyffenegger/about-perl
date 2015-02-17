d487a4eb0a126aabf92531d159f57f2e9c6d40f3
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-02-17 07:05:02 +0100

    In place editing

--- functions/for.pl
+++ functions/for.pl
@@ -11,3 +11,16 @@ for (my $id=3; $id<10; $id++) {
 say 'print 10 random numbers';
 
 say rand for (1 .. 10);
+
+# In place editing:
+
+my @list = qw (abc def ghi);
+
+for my $elem (@list) {
+  $elem .= "-$elem";
+}
+
+print join "\n", @list;
+# abc-abc
+# def-def
+# ghi-ghi
