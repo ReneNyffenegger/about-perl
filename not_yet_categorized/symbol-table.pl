eb8aeca858f0f33e4bc67a88693dac18501c65ae
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-12-17 18:12:06 +0100

    + symbol-table.pl

---
+++ not_yet_categorized/symbol-table.pl
@@ -0,0 +1,9 @@
+use warnings;
+use strict;
+
+my  $VAR_ONE = 'foo'; # Not in symbol table
+our $VAR_TWO = 'bar'; #     in symbol table
+
+for my $symbol (sort keys %main::) {
+  printf "%-50s: %s\n", $symbol, $main::{$symbol};
+}
