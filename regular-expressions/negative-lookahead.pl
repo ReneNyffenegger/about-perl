d8332ac56d8bb543c4fdb5f2b9adb55d94bc7a48
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-01-31 10:54:33 +0100

    + look-around-assertions.pl -> negative-lookahead.pl

--- regular-expressions/negative-lookahead.pl
+++ regular-expressions/negative-lookahead.pl
@@ -1,3 +1,4 @@
+#!/usr/bin/perl
 use warnings;
 use strict;
 
@@ -6,6 +7,9 @@ print "Zero-width negative look-ahead assertion\n\n";
 
 quote_ampersands("foo bar & baz"); # Replace this one
 quote_ampersands("10 &lt; 20"   ); # Don't replace this one
+quote_ampersands("et&amp;"      ); # Don't replace this one
+quote_ampersands("4 &gt; 2"     ); # Don't replace this one
+quote_ampersands("abc&gtef"     ); # Replace this one
 
 sub quote_ampersands {
   my $text = shift;
