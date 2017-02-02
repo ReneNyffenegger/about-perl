7b8fcf3347ff0c05a61c56f168c003e8934885a6
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-02-02 18:00:06 +0100

    print output, too

--- functions/unpack.pl
+++ functions/unpack.pl
@@ -1,6 +1,9 @@
+#!/usr/bin/perl
 use warnings;
 use strict;
 
 my $string = 'ABC';
 
 print "Hexadecimal representation of $string: " . unpack('H*', $string) . "\n";
+#
+# Hexadecimal representation of ABC: 414243
