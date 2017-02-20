2011d0ded6a211f66023ffe8e9ca95d7ca81eb4c
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-02-20 10:42:51 +0100

    Using \k to refer to a named portion ...

--- regular-expressions/named-groups.pl
+++ regular-expressions/named-groups.pl
@@ -4,6 +4,8 @@ use strict;
 
 my $text = 'foo 42 baz';
 
+# Naming a matched portion:
+#
 $text =~ /(?<num>\d+)/;
 print "number found: $+{num}\n";
 #
@@ -13,9 +15,21 @@ print "number found: $+{num}\n";
 $text = 'abc foo OF bar and baz IN foo etc';
 
 # Two named groups with the same name (»where«):
+#
 while ($text =~ /foo OF (?<where>\w+)|(?<where>\w+) IN foo/g) {
   print "where: $+{where}\n";
 }
 #
 # where: bar
 # where: baz
+
+
+$text = 'ab <TAG-ONE>cd efg<TAG-TWO>hi jkl</TAG-TWO>mn opq<TAG-THREE>rst</TAG-THREE>uv</TAG-ONE>wx<TAG-FOUR>yz</TAG-FOUR>';
+
+# Using \k'name' to refer to a named match in the pattern where
+# the name was defined.
+#
+$text =~ m|<(?<tagname>[^>]+)>(?<content>.*)</\k'tagname'|;
+print "$+{content}\n";
+#
+# cd efg<TAG-TWO>hi jkl</TAG-TWO>mn opq<TAG-THREE>rst</TAG-THREE>uv
