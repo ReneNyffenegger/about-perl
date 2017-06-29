e16b98128a3334bbeb51552ee4f1bf17bdc5e16d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-06-29 14:26:05 +0200

    //=

--- operators/slash_slash.pl
+++ operators/slash_slash.pl
@@ -12,3 +12,15 @@ say $p // "bar"; # bar
 say $q // "bar"; # foo
 say $r // "bar"; #
 say $t // "bar"; # 0
+
+say "----------";
+
+$p //= "V";
+$q //= "V";
+$r //= "V";
+$t //= "V";
+
+say $p; # v
+say $q; # foo
+say $r; #
+say $t; # 0
