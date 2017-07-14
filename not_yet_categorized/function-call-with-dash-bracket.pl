7829bce564e3b6ca9fe2131e90cd5283641bf84a
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-14 07:27:00 +0200

    + function-call-with-dash-bracket.pl

---
+++ not_yet_categorized/function-call-with-dash-bracket.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+
+TQ84::Func('one', 2, 'III');
+
+TQ84->Func('bar', 'baz');
+
+my $package = 'TQ84';
+$package->Func('hello','world');
+
+my $blessed = bless {}, 'TQ84';
+print "blessed = $blessed\n";
+$blessed->Func('blessed', '...');
+
+package TQ84;
+
+sub Func {
+  my $arg_1 = shift;
+  my $arg_2 = shift;
+  my $arg_3 = shift;
+
+  print "Func
+  arg_1: $arg_1
+  arg_2: $arg_2
+  arg_3: $arg_3
+
+";
+
+}
+
+1;
