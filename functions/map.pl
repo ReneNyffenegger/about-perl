11fcfe26122e66452200c0adb070abeef1c38b3b
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-05-18 19:20:38 +0200

    755, shebang

--- functions/map.pl
+++ functions/map.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/perl
+
 #
 #   Note the special parsing rule for map:
 #   curly braces don't create a hash.
@@ -7,14 +9,27 @@ use strict;
 
 my @words = qw(one two three four five);
 
+#
+# Use map to create uppercase versions of the
+# elements in a list:
+#
 my @uppercase_words = map {uc} @words;
 
 print join "\n", @uppercase_words;
+#
+# ONE
+# TWO
+# THRE
+# FOUR
+# FIVE
 
-# --
+# -------------------------------------
 
 print "\n--\n";
 
+#
+# Check each element and return a translation if available
+#
 my @words_german = map { $_ eq 'one'   and 'eins' or
                          $_ eq 'two'   and 'zwei' or
                          $_ eq 'three' and 'drei' or
@@ -22,16 +37,9 @@ my @words_german = map { $_ eq 'one'   and 'eins' or
                         '?'}  @words;
 
 print join "\n", @words_german;
-
-__END__
-ONE
-TWO
-THRE
-FOUR
-FIVE
---
-eins
-zwei
-drei
-four
-?
+#
+# eins
+# zwei
+# drei
+# four
+# ?
